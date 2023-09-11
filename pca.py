25f126cc1b335ff8d950b39d36b6f0b2705fc4de
Ajith Senthil <ajithksenthil@gmail.com>
2023-09-11 00:22:20 -0500

    added the first exercise, pca

---
+++ pca.py
@@ -0,0 +1,3 @@
+# principal component analysis (PCA) https://medium.com/accel-ai/pca-algorithm-tutorial-in-python-93ff19212026
+
+# PCA can be done with eigendecomposition and reducing the dimensionality of data. 
